b720d2ff9fb8402f97db3253558b3e3c639ad187
KarisAya <1048827424@qq.com>
2023-02-04 14:07:58 +0800

    新增透群友记录

--- config.py
+++ config.py
@@ -2,9 +2,11 @@ from pydantic import BaseModel, Extra
 
 class Config(BaseModel, extra=Extra.ignore):
     waifu_cd_bye :int = 3600
-    waifu_save:bool = False
+    waifu_save:bool = True
+    waifu_reset:bool = True
     waifu_he :int = 25
     waifu_be :int = 25
     waifu_ntr :int = 20
     yinpa_he :int = 50
     yinpa_be :int = 0
+    yinpa_cp :int = 65
