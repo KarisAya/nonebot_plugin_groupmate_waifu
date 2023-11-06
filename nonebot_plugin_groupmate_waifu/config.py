a1aa53d14636d4a6b8291c1a18eb8696b4d02318
KarisAya <1048827424@qq.com>
2023-11-06 12:54:56 +0800

    修复群友卡池

--- nonebot_plugin_groupmate_waifu/config.py
+++ nonebot_plugin_groupmate_waifu/config.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel, Extra
 
+
 class Config(BaseModel, extra=Extra.ignore):
-    waifu_cd_bye :int = 3600
-    waifu_save :bool = True
-    waifu_reset :bool = True
-    waifu_he :int = 40
-    waifu_be :int = 20
-    waifu_ntr :int = 50
-    yinpa_he :int = 50
-    yinpa_be :int = 0
-    yinpa_cp :int = 80
-    waifu_last_sent_time_filter :int = 2592000
+    waifu_cd_bye: int = 3600
+    waifu_save: bool = True
+    waifu_reset: bool = True
+    waifu_he: int = 40
+    waifu_be: int = 20
+    waifu_ntr: int = 50
+    yinpa_he: int = 50
+    yinpa_be: int = 0
+    yinpa_cp: int = 80
+    waifu_last_sent_time_filter: int = 2592000
--- nonebot_plugin_groupmate_waifu/utils.py
+++ nonebot_plugin_groupmate_waifu/utils.py
@@ -3,8 +3,9 @@ import httpx
 import hashlib
 import asyncio
 
-from pil_utils import BuildImage,Text2Image
+from pil_utils import BuildImage, Text2Image
 from nonebot.adapters.onebot.v11 import Message
+
 
 async def download_avatar(user_id: int) -> bytes:
     url = f"https://q1.qlogo.cn/g?b=qq&nk={user_id}&s=640"
@@ -13,6 +14,7 @@ async def download_avatar(user_id: int) -> bytes:
         url = f"https://q1.qlogo.cn/g?b=qq&nk={user_id}&s=100"
         data = await download_url(url)
     return data
+
 
 async def download_url(url: str) -> bytes:
     async with httpx.AsyncClient() as client:
@@ -25,41 +27,50 @@ async def download_url(url: str) -> bytes:
                 await asyncio.sleep(3)
     raise Exception(f"{url} 下载失败！")
 
+
 async def download_user_img(user_id: int):
     data = await download_avatar(user_id)
     img = BuildImage.open(io.BytesIO(data))
     return img.save_png()
 
+
 async def user_img(user_id: int) -> bytes:
-    '''
+    """
     获取用户头像url
-    '''
+    """
     url = f"https://q1.qlogo.cn/g?b=qq&nk={user_id}&s=640"
     data = await download_url(url)
     if hashlib.md5(data).hexdigest() == "acef72340ac0e914090bd35799f5594e":
         url = f"https://q1.qlogo.cn/g?b=qq&nk={user_id}&s=100"
     return url
 
+
 def text_to_png(msg):
-    '''
+    """
     文字转png
-    '''
+    """
     output = io.BytesIO()
-    Text2Image.from_text(msg,50,spacing = 10).to_image("white",(20,20)).save(output, format="png")
+    Text2Image.from_text(msg, 50, spacing=10).to_image("white", (20, 20)).save(
+        output, format="png"
+    )
     return output
+
 
 def bbcode_to_png(msg, spacing: int = 10):
-    '''
+    """
     bbcode文字转png
-    '''
+    """
     output = io.BytesIO()
-    Text2Image.from_bbcode_text(msg, 50, spacing = spacing).to_image("white", (20,20)).save(output, format="png")
+    Text2Image.from_bbcode_text(msg, 50, spacing=spacing).to_image(
+        "white", (20, 20)
+    ).save(output, format="png")
     return output
 
-def get_message_at(message:Message) -> list:
-    '''
+
+def get_message_at(message: Message) -> list:
+    """
     获取at列表
-    '''
+    """
     qq_list = []
     for msg in message:
         if msg.type == "at":
