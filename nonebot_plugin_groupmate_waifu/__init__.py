d4daf1d1f713746ab24d159101a8d1f130be7153
KarisAya <1048827424@qq.com>
2024-04-27 16:27:44 +0800

    clovers (0.1.7)

--- nonebot_plugin_groupmate_waifu/__init__.py
+++ nonebot_plugin_groupmate_waifu/__init__.py
@@ -1,8 +1,5 @@
-from clovers.core.plugin import PluginLoader
-from nonebot_plugin_clovers import clovers
-from nonebot.log import logger
 from nonebot.plugin import PluginMetadata
-
+from nonebot_plugin_clovers import clovers
 __plugin_meta__ = PluginMetadata(
     name="娶群友",
     description="娶一个群友做老婆",
@@ -11,18 +8,5 @@ __plugin_meta__ = PluginMetadata(
     homepage="https://github.com/KarisAya/nonebot_plugin_groupmate_waifu",
     supported_adapters={"nonebot.adapters.onebot.v11"},
 )
-
-
-def load_plugin(name: str):
-    plugin = PluginLoader.load(name)
-    if plugin is None:
-        logger.error(f"未找到{name}")
-    elif plugin not in clovers.plugins:
-        clovers.plugins.append(plugin)
-        logger.success(f"{name}加载成功")
-    else:
-        logger.success(f"{name}已存在")
-
-
-load_plugin("clovers_apscheduler")
-load_plugin("clovers_groupmate_waifu")
+clovers.load_plugin("clovers_apscheduler")
+clovers.load_plugin("clovers_groupmate_waifu")
